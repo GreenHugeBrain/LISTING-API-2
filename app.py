821e01ba1fb862463ca783c43916c63b65851b14
GreenHugeBrain <142096498+GreenHugeBrain@users.noreply.github.com>
2024-09-05 20:59:27 +0400

    Update app.py

--- app.py
+++ app.py
@@ -117,4 +117,4 @@ if __name__ == "__main__":
     deletion_thread = threading.Thread(target=delete_data_every_2_minutes)
     deletion_thread.start()
 
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
